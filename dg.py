14cd852bf704a47b1e265dc8b54275568dbeb42b
Miłosz Szewczak <milek@milek.eu>
2021-05-07 09:44:43 +0200

    Update dg.py
    fix1

--- dg.py
+++ dg.py
@@ -6,7 +6,7 @@ import re
 import sys
 import os
 import json
-from datetime import datetime
+import datetime
 from elasticsearch import Elasticsearch
 import hashlib
 
@@ -14,21 +14,18 @@ import hashlib
 def only_numerics(seq):
     seq_type= type(seq)
     return seq_type().join(filter(seq_type.isdigit, seq))
-  
-es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
-
 
 HEADERS = ({'User-Agent':
             'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36',
             'Accept-Language': 'en-US, en;q=0.5'})
-
 page = requests.get("https://dreamcargiveaways.co.uk/current-competitions",headers=HEADERS)
 soup = BeautifulSoup(page.content, 'html.parser')
 
 t1=soup.find_all('li', {"class" : re.compile('product type-product post.*')} )
 
+es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
 
-tt=datetime.now()
+tt=datetime.datetime.now().isoformat()
 
 for num in t1:
     link = [a['href'] for a in  num.find_all('a',    {"class" : re.compile('woocommerce-LoopProduct-link woocommerce-loop-product__link')}  )  ][0]
@@ -57,7 +54,6 @@ for num in t1:
         price=  t1.find_all('span', {"class" : re.compile('woocommerce-Price-amount amount')} )[0].get_text()[1:]
         label=  t1.find_all('label', {"class" : re.compile('sr-only')} )[0].get_text() 
         label = os.linesep.join([s for s in label.splitlines() if s]).lstrip()
-        tt=datetime.now()
         x = {
         "label": label,
         "timeleft":    int(timed)*60*60*24  +    int(timeh)*60*60 +     int(timem)*60 + int(times)  ,
@@ -65,7 +61,7 @@ for num in t1:
         "ticketssold": int(ticketssold),
         "tickerprice": float(price),
         "link": link,
-        "timestamp": str(tt) 
+        "@timestamp": tt
         }
         y = json.dumps(x)
         print(y) 
